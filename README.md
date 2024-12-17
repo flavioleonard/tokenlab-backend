62e5f47469608f050603998bd2eea1d6e7630f80
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2024-12-17 13:08:48 -0300

    criacao-usuario-e-useralrearyexists

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tokenlab-backend
 Backend do projeto calendario
+
+[X] Criar usuário 
+[] Checar se o usuario ja existe
+[] Listar os usuarios 
+[] Buscar os usuarios pelo id
+[] Atualizar usuario
+[] Tornar usuario um admin 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,41 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import * as express from "express";
+import {v4 as uuidv4} from "uuid"
 
-dotenv.config();
+const app = express.default(); // Use express.default()
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+interface User {
+  id: string;
+  username: string;
+}
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+const usersBase: User[] = [];
+
+// Criar usuário
+app.post('/users', (req: express.Request, res: express.Response) => {
+  const { username } = req.body;
+  const id = uuidv4();
+
+  const userAlreadyExists = usersBase.some((user) => user.username === username); //método some verifica se nesse array existe algum username que com esse username
+
+  if (userAlreadyExists){
+    return res.status(400).json({error: "User already exists"});
+  }
+
+  usersBase.push({
+    id,
+    username,
+  });
+
+  return res.status(201).json(usersBase);
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Servidor está rodando!");
 });
+
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
