34fc3f3c11b3647050928f15176fb1c7e3eefc27
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2024-12-17 19:45:23 -0300

    middleware-findById-check

--- src/middlewares/findUserById.ts
+++ src/middlewares/findUserById.ts
@@ -2,5 +2,13 @@ import * as express from "express";
 import { User } from "../@types/User";
 
 export function findUserById(usersBase: User[]) {
-    
+    return(req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const { userId } = req.params;
+        const user = usersBase.find((user) => user.id === userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        req.body.user = user;
+        return next();
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { v4 as uuidv4 } from "uuid";
 import { checkUserAlreadyExists } from "./middlewares/checkUsersExists";
 import { User } from "./@types/User";
+import { findUserById } from "./middlewares/findUserById";
 
 const app = express.default(); // Use express.default()
 
@@ -31,27 +32,14 @@ app.get("/users",  (req: express.Request, res: express.Response) => {
 });
 
 // Buscando usuário por ID
-app.get("/users/:userId", (req: express.Request, res: express.Response) => {
-  const { userId } = req.params;
-
-  const user = usersBase.find((user) => user.id === userId);
-
-  if (user) {
+app.get("/users/:userId", findUserById(usersBase), (req: express.Request, res: express.Response) => {
+    const user = req.body.user;
     return res.status(200).json(user);
-  } else {
-    return res.status(400).json({ error: "User not found" });
-  }
 });
 
 // Atualizando usuário
-app.patch('/users/:userId', (req: express.Request, res: express.Response) => {
-  const { userId } = req.params;
-
-  const user = usersBase.find((user) => user.id === userId);
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
-
+app.patch('/users/:userId', findUserById(usersBase), (req: express.Request, res: express.Response) => {
+  const user = req.body.user;
   const { username } = req.body;
   user.username = username;
 
@@ -59,13 +47,8 @@ app.patch('/users/:userId', (req: express.Request, res: express.Response) => {
 });
 
 // Deletar usuário
-app.delete('/users/:userId', (req: express.Request, res: express.Response) => {
-  const { userId } = req.params;
-  const user = usersBase.find((user) => user.id === userId);
-
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
+app.delete('/users/:userId', findUserById(usersBase), (req: express.Request, res: express.Response) => {
+  const user = req.body.user;
 
   const index = usersBase.indexOf(user);
 
